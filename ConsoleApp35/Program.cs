1ae9cf3b2ca47a45322042ce6c1d38a574463fac
MINEDUCYT <MINEDUCYT@DESKTOP-GLJR0B3>
2025-03-28 21:40:40 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+	internal class Program
+	{
+		class Celular
+		{
+			private string contraseña = "1234";
+
+			public void Desbloquear(string intento)
+			{
+				if (intento == contraseña)
+				{
+					Console.WriteLine("Celular desbloqueado.");
+				}
+				else
+				{
+					Console.WriteLine("Contraseña incorrecta.");
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Celular miCelular = new Celular();
+
+			Console.Write("Ingrese la contraseña: ");
+			string intento = Console.ReadLine();
+
+			miCelular.Desbloquear(intento);
+
+			Console.ReadKey(); // Para que la consola no se cierre inmediatamente
+		}
+	}
+}
---
+++ ConsoleApp35/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleApp35")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp35")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f048b317-3c21-42a7-955a-93b44a54a30a")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
